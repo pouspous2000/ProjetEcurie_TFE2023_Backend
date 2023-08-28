3268e53a39d246e80e40cb94a0103b2fdac0ed34
Ubuntu <ubuntu@ip-172-31-36-83.ec2.internal>
2023-08-28 09:52:10 +0000

    BACK_deploiement_[ADD]_[deploiement front & back]

--- src/configuration/sequelize.js
+++ src/configuration/sequelize.js
@@ -4,8 +4,8 @@ new Dotenv()
 
 const defaultConfig = {
 	dialect: 'postgres',
-	database: process.env.DATABASE_NAME,
 	host: process.env.DATABASE_HOST,
+	database: process.env.DATABASE_NAME,
 	port: process.env.DATABASE_PORT,
 	username: process.env.DATABASE_USERNAME,
 	password: process.env.DATABASE_PASSWORD,
