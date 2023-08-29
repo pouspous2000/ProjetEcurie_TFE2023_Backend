c4adaaeb223e16d24f903f6e20f70519bc23320a
pouspous2000 <63249471+pouspous2000@users.noreply.github.com>
2023-08-29 19:42:49 +0200

    BACK_script_[FIX]_[SCRIPT_SH5]

--- start.sh
+++ start.sh
@@ -7,6 +7,6 @@ backend_dir="$1"
 frontend_repo="https://github.com/pouspous2000/ProjetEcurie_TFE2023_Front.git"
 cd "$backend_dir"
 git clone "$frontend_repo" frontend
-cd ..
+cd "$backend_dir"
 docker-compose -f docker-compose_dev.yml build
 docker-compose -f docker-compose_dev.yml down && docker-compose f docker-compose_dev.yml up
