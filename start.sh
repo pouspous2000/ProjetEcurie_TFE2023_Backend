229bbc8645b055311e3cbab14c4657d5a717fc8b
pouspous2000 <63249471+pouspous2000@users.noreply.github.com>
2023-08-29 20:32:01 +0200

    BACK_script_[FIX]_[SCRIPT_SH8]

--- start.sh
+++ start.sh
@@ -5,8 +5,12 @@ fi
 
 backend_dir="$1"
 frontend_repo="https://github.com/pouspous2000/ProjetEcurie_TFE2023_Front.git"
+if [ -d "$backend_dir/frontend" ]; then
+  echo "Le répertoire frontend existe déjà. Suppression en cours..."
+  rm -rf "$backend_dir/frontend"
+fi
 cd "$backend_dir"
 git clone "$frontend_repo" frontend
 cd "$backend_dir"
 docker-compose -f docker-compose_dev.yml build
-docker-compose -f docker-compose_dev.yml down && docker-compose f docker-compose_dev.yml up
+docker-compose -f docker-compose_dev.yml down && docker-compose f docker-compose_dev.yml up -d
