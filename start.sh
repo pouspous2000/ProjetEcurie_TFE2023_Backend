76595b6a501755ff72764cebaa06dc48f324b5f7
pouspous2000 <63249471+pouspous2000@users.noreply.github.com>
2023-08-29 19:26:43 +0200

    BACK_dev_[FIX]_[SCRIPT_SH2]

--- start.sh
+++ start.sh
@@ -7,9 +7,5 @@ backend_dir="$1"
 frontend_repo="https://github.com/pouspous2000/ProjetEcurie_TFE2023_Front.git"
 cd "$backend_dir"
 git clone "$frontend_repo" frontend
-cd frontend
-npm install
-npm run build
-cd "$backend_dir"
 docker-compose -f docker-compose_dev build
 docker-compose -f docker-compose_dev down && docker-compose f docker-compose_dev up
