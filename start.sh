8a72201bfbe5647125f2aa5e83815fd789af599c
pouspous2000 <63249471+pouspous2000@users.noreply.github.com>
2023-08-30 02:44:01 +0200

    BACK_script_[FIX]_[FIXING .SH]

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 if [ -z "$1" ]; then
   echo "Usage: ./nom_du_script.sh <chemin_vers_backend>"
-  exit 1
+  exit 
 fi
 
 backend_dir="$1"
@@ -9,3 +9,5 @@ git clone "$frontend_repo" frontend
 cd "$backend_dir"
 docker-compose -f docker-compose_dev.yml build
 docker-compose -f docker-compose_dev.yml down && docker-compose -f docker-compose_dev.yml up -d
+
+echo "Le script s'est exécuté avec succès ! Vous avez a présent accès à l'application : http://localhost:3000/"
